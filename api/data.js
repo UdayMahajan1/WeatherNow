5345e8286c20c5c919bf02f9cefd7662f0a99109
Paper Gremlin <mahajanuday144@gmail.com>
2023-07-14 23:40:53 +0530

    almost done

---
+++ api/data.js
@@ -0,0 +1,35 @@
+require('dotenv').config()
+const https = require('node:https')
+const appKey = process.env.APP_KEY
+
+const fetchData = (cityName) => {
+  return new Promise((res, rej) => {
+
+      var url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + appKey + "&units=metric"
+
+      https.get(url, (resp) => {
+
+          resp.on('data', (d) => {
+              var weatherData = JSON.parse(d)
+              if (resp.statusCode === 404) {
+                  console.log("The city doesn't exist in our records. Sorry.")
+                  rej(new Error("The city doesn't exist in our records. Sorry."))
+
+              } else {
+                  var data = {
+                      cityName: cityName,
+                      temp: weatherData.main.temp,
+                      weatherDesc: weatherData.weather[0].description,
+                      icon: weatherData.weather[0].icon
+                  }
+                  res(data)
+              }
+          })
+      }).on('error', (e) => {
+          console.log(e)
+      })
+
+  })
+}
+
+module.exports = fetchData
--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-require('dotenv').config()
 const express = require("express")
 const port = 3000
 const app = express()
-const https = require('node:https')
 const ejs = require('ejs')
-const appKey = process.env.APP_KEY
+const fetchData = require('./api/data')
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true }))
@@ -12,36 +10,6 @@ app.use(express.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
     const initialCities = ["Mumbai", "London", "New York", "Moscow"]
     var initialData = []
-
-    const fetchData = (cityName) => {
-        return new Promise((res, rej) => {
-
-            var url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + appKey + "&units=metric"
-
-            https.get(url, (resp) => {
-
-                resp.on('data', (d) => {
-                    var weatherData = JSON.parse(d)
-                    if (resp.statusCode === 404) {
-                        console.log("The city doesn't exist in our records. Sorry.")
-                        rej(new Error("The city doesn't exist in our records. Sorry."))
-
-                    } else {
-                        var data = {
-                            cityName: cityName,
-                            temp: weatherData.main.temp,
-                            weatherDesc: weatherData.weather[0].description,
-                            icon: weatherData.weather[0].icon
-                        }
-                        res(data)
-                    }
-                })
-            }).on('error', (e) => {
-                console.log(e)
-            })
-
-        })
-    }
 
     const getData = async () => {
         const promises = initialCities.map(async (cityName) => {
@@ -57,32 +25,10 @@ app.get('/', (req, res) => {
     getData()
 })
 
-app.post('/getWeather', (req, res) => {
-    console.log(req.params)
-
+app.post('/getWeather', async (req, res) => {
     const cityName = req.body.cityName
-
-    var url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + appKey + "&units=metric"
-
-    https.get(url, (resp) => {
-        resp.on('data', (d) => {
-            var weatherData = JSON.parse(d)
-            if (resp.statusCode === 404) {
-                res.send("The city doesn't exist in our records. Sorry.")
-            } else {
-                var temp = weatherData.main.temp
-                var weatherDesc = weatherData.weather[0].description
-                var icon = weatherData.weather[0].icon
-                res.write("<h1>The temperature in " + cityName + " is " + temp + " degree celcius.</h1>")
-                res.write("<p>Weather description : " + weatherDesc + "</p>")
-                res.write("<img src = http://openweathermap.org/img/wn/" + icon + "@2x.png>")
-                res.send()
-                // console.log(weatherData) 
-            }
-        })
-    }).on('error', (e) => {
-        console.log(e)
-    })
+    const data = await fetchData(cityName)
+    res.render('getWeather', { city: data })
 })
 
-app.listen(port, () => console.log("Server is running on port 3000")) 
+app.listen(port, () => console.log(`Server is running on port ${port}`)) 
